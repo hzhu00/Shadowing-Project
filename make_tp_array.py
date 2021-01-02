71d77149b4ccd998789d3014442ee2ead90d3b36
hzhu00 <60085555+hzhu00@users.noreply.github.com>
2021-01-02 21:20:21 +0800

    Add files via upload

---
+++ make_tp_array.py
@@ -0,0 +1,65 @@
+import PIL.Image as Image
+import os
+import random
+
+# The current program is only for TP condition.
+
+class array():
+    def __init__(self):
+        self.foilnum = int(input("How many foils? (5/9/15)\n"))
+        self.usedfoils = []
+        self.row = 2
+        if self.foilnum == 5:
+            self.col = 3
+        elif self.foilnum == 9:
+            self.col = 5
+        elif self.foilnum == 15:
+            self.col = 8
+        self.arraynum = int(input("How many arrays for this foil number?\n"))
+        self.impath = "C:\\Users\\haoze\\Desktop\\Images for experiment\\"
+        self.target = random.randint(1, 200)
+        self.targetpath = self.impath + str(self.target) + ".png"
+        self.imheight = 1200
+        self.imwidth = int((512 / 1200) * self.imheight)
+        self.resultname = "Array" + str(self.foilnum) + ".png"
+
+    def change_imheight(self, new_height):
+        self.imheight = new_height
+        self.imwidth = int((512 / 1200) * self.imheight)
+
+    def get_foils(self):
+        foils = []
+        for j in range(self.foilnum):
+            foils.append(str(random.choice([i for i in range(1, 201) if i not in [self.target]+self.usedfoils])))
+        for i in foils:
+            self.usedfoils.append(int(i))
+        return foils
+
+    def list_image_names(self):   
+        imlist = self.get_foils()
+        tarindex = random.randint(0, self.foilnum + 1)
+        imlist.insert(tarindex, str(self.target))
+        return imlist
+
+    def change_bg(self, im):
+        x, y = im.size
+        p = Image.new('RGBA', im.size, "white")
+        p.paste(im, (0, 0, x, y), im)
+        return p
+
+    def image_compose(self, resultname):
+        image_names = self.list_image_names()
+        to_image = Image.new('RGB', (self.col * self.imwidth, self.row * self.imheight), "white")
+        for y in range(1, self.row + 1):
+            for x in range(1, self.col + 1):
+                from_image = Image.open(self.impath + image_names[self.col * (y - 1) + x - 1] + ".png").resize(
+                    (self.imwidth, self.imheight), Image.ANTIALIAS)
+                to_image.paste(self.change_bg(from_image), ((x - 1) * self.imwidth, (y - 1) * self.imheight))
+        return to_image.save(resultname)
+
+    def repeat_compose(self):
+        for n in range(self.arraynum):
+            a.image_compose(str(n+1) + ".png")
+
+a = array()
+a.repeat_compose()
